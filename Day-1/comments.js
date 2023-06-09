6997fcadc14ced6421849f381cb026abeb387930
Gaurav Jani <95155509+itsgauravv@users.noreply.github.com>
2023-06-09 20:25:54 +0530

    Add files via upload

---
+++ Day-1/comments.js
@@ -0,0 +1,7 @@
+// comments can make code readable.
+
+// Welcome to 30DaysofJavaScript
+
+/*
+comments can make code readable, easy to use and informative
+*/
---
+++ Day-1/datatypes.js
@@ -0,0 +1,36 @@
+// Type of each datatype.
+console.log(typeof 1);
+console.log(typeof "welcome");
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Declaring without values.
+let a;
+let b;
+let c;
+let d;
+
+// Declaring with values.
+let p = 123;
+let q = "hey";
+let r = true;
+let s = "hi";
+
+// Storing in multiple lines.
+let firstName = "Gaurav";
+let lastName = "Jani";
+let maritalStatus = "unmarried";
+let country = "India";
+let age = 19;
+
+// Storing in one line.
+let Firstname = "Gaurav",
+  Lastname = "Jani",
+  Maritalstatus = "unmarried",
+  Country = "India",
+  Age = 19;
+
+let myAge = `I am 19 years old.`;
+let yourAge = `You am 21 years old.`;
+console.log(myAge);
+console.log(yourAge);
